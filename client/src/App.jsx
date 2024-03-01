6161d5af6c8bf3db9b470689fd861f1a5d2b188e
Edsong158 <150963218+Edsong158@users.noreply.github.com>
2024-03-01 17:38:27 -0500

    2 pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,5 +4,18 @@ import { useState } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import CreateAccount from './pages/CreateAccount'
+import UserReviews from './pages/UsersReviews'
+import SeeMoreReview from './pages/SeeMoreReview'
+
+function App() {
+    const [isDarkMode, setIsDarkMode] = useState(true)
+
+    return (
+        <div className={`main-content ${isDarkMode}`}></div>
+    )
+}
+
+
 
 export default App;
