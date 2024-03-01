8f3d60c98b92179b8f414ed884a245353715bf41
Freddy Lau <freddylaudesign@gmail.com>
2024-03-01 14:08:07 -0500

    server-setup

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,42 +1,16 @@
-const { createToken, verifyToken, protect } = require('../../config/auth');
-const { GraphQLError, GraphQLString } = require('graphql');
-const { User } = require('../../models');
+const user_resolvers = require('./lib/user_resolvers');
+const review_resolvers = require('./lib/note_resolvers');
 
-module.exports = {
-  queries: {
-    async authenticate(_, __, { req }) {
-      const token = req.cookies.token;
+const resolvers = {
+  Query: {
+    ...user_resolvers.queries,
+    ...review_resolvers.queries,
+  },
 
-      if (!token) return null;
-
-      try {
-        const user_id = verifyToken(token);
-
-        const user = await User.findById(user_id);
-
-        return user;
-      } catch (err) {
-        console.log(err);
-        return null;
-      }
-    },
+  Mutation: {
+    ...user_resolvers.mutations,
+    ...review_resolvers.mutations,
   },
 };
 
-// From JD MERN
-// const user_resolvers = require('./lib/user_resolvers')
-// const note_resolvers = require('./lib/note_resolvers')
-
-// const resolvers = {
-//   Query: {
-//     ...user_resolvers.queries,
-//     ...note_resolvers.queries,
-//   },
-
-//   Mutation: {
-//     ...user_resolvers.mutations,
-//     ...note_resolvers.mutations,
-//   }
-// }
-
-// module.exports = resolvers
+module.exports = resolvers;
